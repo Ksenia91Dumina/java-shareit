eb1f01f8298b6d84ee82a7abfdb9a1dd1390c90d
Ksenia Dumina <dum-dum@mail.ru>
2022-10-08 14:38:49 +0300

    add-controllers

--- src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -32,22 +31,9 @@ public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.FORBIDDEN)
     public ErrorResponse handleValidationException(final NotAllowedException e) {
-        log.info("500 {}", e.getMessage(), e);
+        log.info("403 {}", e.getMessage(), e);
         return new ErrorResponse(e.getMessage());
     }
 
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ErrorResponse handleIfAnyException(final Throwable e) {
-        log.info("500 {}", e.getMessage(), e);
-        return new ErrorResponse(e.getMessage());
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleIfValidationException(final MethodArgumentNotValidException e) {
-        log.info("400 {}", e.getMessage(), e);
-        return new ErrorResponse(e.getMessage());
-    }
 
 }
